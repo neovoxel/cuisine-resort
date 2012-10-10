35f0ff4bed3736f24f2f80b52f0a1dc4e42484b5
Thedahu <thedahu.dragon@gmail.com>
2012-10-10 14:56:08 +0200

    Ajout de la barre de navigation et legere modification dans pied de page

--- site/includes/pied.inc.php
+++ site/includes/pied.inc.php
@@ -1,8 +1,8 @@
 <?php
 $content=<<<EOF
 	 <h2>Plan du site</h2>
-	 <p><a href="./index.php?page=accueil">Accueil</a><br />
-	    <a href="./index.php?page=liste_recette">Liste des recettes</a><br />
+	 <p><a href="./index.php?page=">Accueil</a><br />
+	    <a href="./index.php?page=recettes">Liste des recettes</a><br />
 	    <a href="./index.php?page=profil">Profil</a><br />
 	    <a href="./index.php?page=inscription">Inscription</a></p><br />
 EOF;
