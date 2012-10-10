b15a18933972d7c1624429a09aaef128b726f5b0
Thedahu <thedahu.dragon@gmail.com>
2012-10-10 14:44:11 +0200

    Ajout du pied de page (pied.inc.php)

---
+++ site/includes/pied.inc.php
@@ -0,0 +1,10 @@
+<?php
+$content=<<<EOF
+	 <h2>Plan du site</h2>
+	 <p><a href="./index.php?page=accueil">Accueil</a><br />
+	    <a href="./index.php?page=liste_recette">Liste des recettes</a><br />
+	    <a href="./index.php?page=profil">Profil</a><br />
+	    <a href="./index.php?page=inscription">Inscription</a></p><br />
+EOF;
+echo $content;
+?>
