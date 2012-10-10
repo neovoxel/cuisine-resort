845c41d0b9c35349ac5ebcd109c4e91bbbfefabe
neovoxel <neovoxel@gmail.com>
2012-10-10 14:42:05 +0200

    J'ai commencé le codage de index layout et entete.

---
+++ site/includes/error_404.inc.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+<body>
+	<h1>Erreur 404 : Page non trouvée</h1>
+</body>
+</html>
--- site/index.php
+++ site/index.php
@@ -1,1 +1,8 @@
+<?php
+	$_SQUELETTE['entete'] = loadPageContent('entete.inc.php');
+	$_SQUELETTE['navigation'] = loadPageContent('navigation.inc.php');
+	$_SQUELETTE['pied'] = loadPageContent('pied.inc.php');
+	
+	render($_SQUELETTE, 'layout.inc.php');
 
+?>
