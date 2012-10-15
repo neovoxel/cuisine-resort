2c5675c3080a77c845af13957123eca9ffd5f066
neovoxel <neovoxel@gmail.com>
2012-10-15 15:56:11 +0200

    Ajout des images des catégories.

--- site/index.php
+++ site/index.php
@@ -6,16 +6,16 @@
 	if (isset($_GET['page']) and !empty($_GET['page']))
 	{
 		if (pageExists($_GET['page'], $_PAGES))
-			$page = $_PAGES[$_GET['page']];
+			$contenu_page = $_PAGES[$_GET['page']];
 		else
-			$page = ERROR_404;
+			$contenu_page = ERROR_404;
 	}
 	else
-		$page = $_PAGES['accueil'];
+		$contenu_page = $_PAGES['accueil'];
 	
 	$_SQUELETTE['entete']		= loadPageContent('includes/entete.inc.php');
 	$_SQUELETTE['navigation']	= loadPageContent('includes/navigation.inc.php');
-	$_SQUELETTE['contenu']		= loadPageContent($page);
+	$_SQUELETTE['contenu']		= loadPageContent($contenu_page);
 	$_SQUELETTE['pied']			= loadPageContent('includes/pied.inc.php');
 	
 	render($_SQUELETTE, 'includes/layout.inc.php');
