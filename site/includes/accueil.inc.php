296f82b323e1cd211b5f04fdb9cf756f23c583bd
neovoxel <neovoxel@gmail.com>
2012-10-24 01:31:19 +0200

    Ajout de la page "liste des catégories" et "listage des recettes par catégorie".

--- site/includes/accueil.inc.php
+++ site/includes/accueil.inc.php
@@ -22,68 +22,8 @@ catch (Exception $err)
 if ($result_recettes->rowCount() > 0) {
 	$result_recettes = $result_recettes->fetchAll(PDO::FETCH_ASSOC);
 	
-	foreach ($result_recettes as $line) {
-		$id_recette = $line['id_recette'];
-		
-		$request_utlisateur = <<< EOF
-		SELECT U.id_utilisateur, login, nom_utilisateur, prenom
-		FROM recette R INNER JOIN utilisateur U ON R.id_utilisateur=U.id_utilisateur
-		WHERE id_recette = $id_recette;
-EOF;
-		$request_categorie = <<< EOF
-		SELECT C.id_categorie, nom_categorie
-		FROM recette R INNER JOIN appartient A ON R.id_recette=A.id_recette
-		INNER JOIN categorie C ON A.id_categorie=C.id_categorie
-		WHERE R.id_recette = $id_recette;
-EOF;
-		
-		try { $result_utlisateur=$PDO_BDD->query($request_utlisateur); }
-		catch (Exception $err)
-		{ die ('Erreur : '.$err->getMessage()); }
-		
-		try { $result_categorie=$PDO_BDD->query($request_categorie); }
-		catch (Exception $err)
-		{ die ('Erreur : '.$err->getMessage()); }
-		
-		
-		if ($result_utlisateur->rowCount() > 0
-		and $result_categorie->rowCount() > 0) {
-			
-			$result_utlisateur	= $result_utlisateur->fetchAll(PDO::FETCH_ASSOC);
-			$result_categorie	= $result_categorie->fetchAll(PDO::FETCH_ASSOC);
-			
-			$categories_recette = 'Catégorie';
-			if (count($result_categorie) > 1)
-				$categories_recette .= 's';
-			$categories_recette .= ' : ';
-			foreach ($result_categorie as $value) {
-				$categories_recette .= '<a href="./index.php?page=recettes&idc='.$value['id_categorie'].'">'.$value['nom_categorie'].'</a> ';
-			}
-			
-			$titre_recette		= $line['titre'];
-			$texte_recette		= substr($line['recette'], 0, 250).'...';
-			$date_recette		= formatDate($line['date_recette']);
-			$nom_utilisateur	= $result_utlisateur[0]['login'];
-			$id_utilisateur		= $result_utlisateur[0]['id_utilisateur'];
-			$lien_page_detail	= '<a href="./index.php?page=detail&idr='.$id_recette.'">';
-			
-			if (is_null($line['image_recette'])) {
-				$image_recette = 'images/default/recette.png';
-			}
-			else
-				$image_recette = 'images/'.$nom_utilisateur.'/'.$titre_recette.'/'.$line['image_recette'];
-			
-			echo <<< EOF
-<div class="recette">
-	<p>$lien_page_detail<img class="img_recette" src="$image_recette" alt="Illustration recette" height="150" width="150" /></a></p>
-	<h3>$lien_page_detail $titre_recette</a></h3>
-	<h4>Le $date_recette par <a href="./index.php?page=profil&idp=$id_utilisateur">$nom_utilisateur</a></h4>
-	<p class="texte_recette">$texte_recette $lien_page_detail Lire la suite</a></p>
-	<p>$categories_recette</p>
-</div>
-EOF;
-		}
-	}
+	foreach ($result_recettes as $line)
+		echo previewRecette($line['id_recette']);
 	
 }
 else
--- site/includes/liste_recettes.inc.php
+++ site/includes/liste_recettes.inc.php
@@ -1,44 +1,80 @@
 <?php
 
 	$jeutest = array('Entrees', 'Plats', 'Dessert');
-	global $PDO_BDD;
-
-	if (isset($_GET['idc']) and !empty($_GET['idc']))
-	{
-		/*
-		 * A faire: Finir la liste des recettes (voir requette)
-		 * */
-		
-		echo "NOPE.avi"; //A modifier avec liste recette de la categorie
-		$liste_recette_request='SELECT * FROM categorie ;';
-		
-		try { $liste_categorie_result=$PDO_BDD->query($liste_recette_request); }
-		catch (Exception $err)
-		{ die ('Erreur : '.$err->getMessage()); }
-
-		$tab_result=$liste_categorie_result->fetchAll(PDO::FETCH_ASSOC);
-		
-		foreach($tab_result as $value)
-			echo '<a href="./index.php?page=recettes&idc='.$value['id_categorie'].'"><div id="'.$value['id_categorie'].'">
-					<div id="img-'.$value['id_categorie'].'"><img src="./images/categories/'.$value['image_categorie'].'"/></div>
-					<div id="txt-'.$value['id_categorie'].'">'.$value['nom_categorie'].' : Mon texte</div>
-				  </div></a>'; //A modifier avec les catégories
-	}
 	
-	else
-	{
-		$liste_recette_request='SELECT * FROM categorie;';
+	$PDO_BDD = getPDO_BDD();
+	$liste_categorie_request='SELECT * FROM categorie ;';
+	
+	try { $liste_categorie_result=$PDO_BDD->query($liste_categorie_request); }
+	catch (Exception $err)
+	{ die ('Erreur : '.$err->getMessage()); }
+
+	$tab_result=$liste_categorie_result->fetchAll(PDO::FETCH_ASSOC);
+	
+	
+	if (isset($_GET['idc']) and !empty($_GET['idc'])) {
+		$my_idc = $_GET['idc'];
 		
-		try { $liste_categorie_result=$PDO_BDD->query($liste_recette_request); }
+		$liste_recettes_request= <<< EOF
+		SELECT id_recette
+		FROM appartient
+		WHERE id_categorie = $my_idc;
+EOF;
+
+		try { $liste_recettes_result=$PDO_BDD->query($liste_recettes_request); }
 		catch (Exception $err)
 		{ die ('Erreur : '.$err->getMessage()); }
-
-		$tab_result=$liste_categorie_result->fetchAll(PDO::FETCH_ASSOC);
 		
-		foreach($tab_result as $value)
-			echo '<a href="./index.php?page=recettes&idc='.$value['id_categorie'].'"><div id="'.$value['id_categorie'].'">
-					<div id="img-'.$value['id_categorie'].'"><img src="./images/categories/'.$value['image_categorie'].'"/></div>
-					<div id="txt-'.$value['id_categorie'].'">'.$value['nom_categorie'].' : Mon texte</div>
-				  </div></a>'; //A modifier avec les catégories
+		if ($liste_recettes_result->rowCount() > 0) {
+			
+			echo '<div id="liste_categories">';
+			
+			foreach($tab_result as $value) {
+				if ($value['id_categorie'] == $my_idc) {
+					$id_categorie		= $value['id_categorie'];
+					$image_categorie	= $value['image_categorie'];
+					$nom_categorie		= $value['nom_categorie'];
+					
+					echo <<< EOF
+<div id="categorie_selection">
+<img class="img_categorie" src="./images/categories/$image_categorie" />
+<h1>$nom_categorie</h1>
+</div>
+EOF;
+
+					$liste_recettes_result = $liste_recettes_result->fetchAll(PDO::FETCH_ASSOC);
+					
+					echo '<div id="liste_recettes">';
+					
+					foreach ($liste_recettes_result as $line)
+						echo previewRecette($line['id_recette']);
+					
+					echo '</div>';
+				}
+			}
+			
+			echo '</div>';
+		}
+		else
+			echo 'Cette catégorie est vide ou n\'existe pas.';
+	}
+	else {
+		echo '<div id="liste_categories">';
+		
+		foreach($tab_result as $value) {
+			$id_categorie		= $value['id_categorie'];
+			$image_categorie	= $value['image_categorie'];
+			$nom_categorie		= $value['nom_categorie'];
+			
+			echo <<< EOF
+<div class="categorie" >
+<a href="./index.php?page=recettes&idc=$id_categorie"><img class="img_categorie" src="./images/categories/$image_categorie" /></a>
+<h1><a href="./index.php?page=recettes&idc=$id_categorie">$nom_categorie</a></h1>
+<p>Mon texte<p>
+</div>
+EOF;
+		}
+		
+		echo '</div>';
 	}
 ?>
