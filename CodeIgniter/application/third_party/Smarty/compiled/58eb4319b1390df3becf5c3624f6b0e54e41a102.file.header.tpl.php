3b55a0995122760a8a9416ae6ed85ba161a8fc1a
Thedahu <thedahu.dragon@gmail.com>
2012-12-01 17:01:41 +0100

    Arrangement des fonctions dans commentaires, changements dans profil.

---
+++ CodeIgniter/application/third_party/Smarty/compiled/58eb4319b1390df3becf5c3624f6b0e54e41a102.file.header.tpl.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2012-12-01 16:59:44
+         compiled from "application/views\header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2159450ba2970c7d647-81859663%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '58eb4319b1390df3becf5c3624f6b0e54e41a102' => 
+    array (
+      0 => 'application/views\\header.tpl',
+      1 => 1354374805,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2159450ba2970c7d647-81859663',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_50ba2970c86a4',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_50ba2970c86a4')) {function content_50ba2970c86a4($_smarty_tpl) {?>
+<a class="brand" href="<?php echo site_url();?>
+"><img src="<?php echo base_url('images/banniere.jpg');?>
+" /></a>
+<?php }} ?>
---
+++ CodeIgniter/application/third_party/Smarty/compiled/bb11a596c651ffe5b7bf9b6d306b8f393feb1a0f.file.footer.tpl.php
@@ -0,0 +1,25 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2012-12-01 16:59:44
+         compiled from "application/views\footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:167550ba2970c91242-58918817%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'bb11a596c651ffe5b7bf9b6d306b8f393feb1a0f' => 
+    array (
+      0 => 'application/views\\footer.tpl',
+      1 => 1354374805,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '167550ba2970c91242-58918817',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_50ba2970c92ec',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_50ba2970c92ec')) {function content_50ba2970c92ec($_smarty_tpl) {?>
+<p>Footer !!!</p>
+
+<?php }} ?>
